41f7f95674dc3bf249c4b9f404146b3724221362
Rebecca Collins <rebeccacollins1007@gmail.com>
2024-07-03 13:22:57 +0100

    Complete

--- FuncActivities.js
+++ FuncActivities.js
@@ -34,13 +34,13 @@
 
 //Activity 2.3
 let balance = 420.6
-let pin = 0408
+let pin = 1408
 const cashWithdrawal = (amount, accNum, pinNum) => {
     if ((pin === pinNum) && (balance>amount))
         {
             console.log(`Withdrawing ${amount} from account ${accNum}`)
         }
 }
-cashWithdrawal(20, 080001056, 0408);
-cashWithdrawal(400, 66666666, 0130);
-cashWithdrawal(666.50, 66666666, 0408);
+cashWithdrawal(20, 180001056, 1408);
+cashWithdrawal(400, 66666666, 5130);
+cashWithdrawal(666.50, 66666666, 1408);
