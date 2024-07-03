2d7a98f74d4d9dd80ffc4255512e05b43397d376
Rebecca Collins <rebeccacollins1007@gmail.com>
2024-07-03 11:23:05 +0100

    save_01

---
+++ FuncActivities.js
@@ -0,0 +1,17 @@
+////Activity 1.1
+// const faveColor = () => {
+//     console.log("My favourite colour is Blue.");
+// };
+// faveColor();
+
+//Activity 2.2
+// const faveColor = (color) => {
+//     console.log(`My favourite colour is ${color}`);
+// };
+// faveColor("Purple");
+
+//Activity 2.1
+
+//Activity 2.2
+
+//Activity 2.3
---
+++ Functions.js
@@ -0,0 +1,39 @@
+// let age = 32;
+// if(age < 18){
+//     console.log("Your ticket is £8")
+// }
+// else if(age >= 18 && age <60){
+//     console.log("Your ticket is £10.95")
+// }
+// else if(age > 60){
+//     console.log("Your ticket is £7.50")
+// }
+// else{
+//     console.log("Invalid Input. Please try again.")
+// }
+
+// const pressGrindBeans = () => {
+//     console.log("Grinding for 20 seconds..");
+// };
+
+// pressGrindBeans();
+
+// let coffeeIsGrinding = false;
+// const pressGrindBeans = () => {
+//     if (coffeeIsGrinding) {
+//         console.log("Stopping the grind..");
+//             coffeeIsGrinding = true;
+//     } else {
+//         console.log("Starting the grind~");
+//         coffeeIsGrinding = false;
+//     };
+// };
+// pressGrindBeans();
+
+// let accnumber = 27596066
+// const cashWithdrawal = (amount, accNum) => {
+//     console.log(`Withdrawing ${amount} from account ${accNum}..`);
+// };
+// cashWithdrawal(300, accnumber);
+// cashWithdrawal(50, 11811800);
+// cashWithdrawal(666, 60606060);
