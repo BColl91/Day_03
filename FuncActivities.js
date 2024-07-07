17f4e718d394037a2017d1b7802e64d965a95b16
Rebecca Collins <rebeccacollins1007@gmail.com>
2024-07-07 11:42:29 +0100

    Complete

--- FuncActivities.js
+++ FuncActivities.js
@@ -4,6 +4,7 @@
 // };
 // faveColor();
 
+
 //Activity 2.2
 // const faveColor = (color) => {
 //     console.log(`My favourite colour is ${color}`);
@@ -31,16 +32,15 @@
 // takeOrder("cheese", "beans");
 
 
-
-//Activity 2.3
-// let balance = 420.6
-// let pin = 1408
-// const cashWithdrawal = (amount, accNum, pinNum) => {
-//     if ((pin === pinNum) && (balance>amount))
-//         {
-//             console.log(`Withdrawing ${amount} from account ${accNum}`)
-//         }
-// }
-// cashWithdrawal(20, 180001056, 1408);
-// cashWithdrawal(400, 66666666, 5130);
-// cashWithdrawal(666.50, 66666666, 1408);
+// Activity 2.3
+let balance = 420.6
+let pin = 1408
+const cashWithdrawal = (amount, accNum, pinNum) => {
+    if ((pin === pinNum) && (balance>amount))
+        {
+            console.log(`Withdrawing ${amount} from account ${accNum}`)
+        }
+}
+cashWithdrawal(20, 180001056, 1408);
+cashWithdrawal(400, 66666666, 5130);
+cashWithdrawal(666.50, 66666666, 1408);
